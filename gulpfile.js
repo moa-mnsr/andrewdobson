2569630ff332cedbc69e55d5b8c1afbfeab8ca9c
M Mansour <mansourmo@tutanota.com>
2021-09-17 23:04:23 +0200

    gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
+sass.compiler = require('node-sass');
 
-});
+gulp.task("sass", function () {
+ // we want to run "sass css/app.scss app.css --watch"
+ return gulp.src("css/app.scss")
+ .pipe(sass())
+ .pipe(gulp.dest("."))
+})
+
+gulp.task('default', ["sass"]);
+
+
+gulp.task("watch", function() {
+    gulp.watch("css/app.scss", ["sass", "watch"])
+})
